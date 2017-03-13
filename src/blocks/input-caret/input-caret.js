810f6496d582ec538c4c64604b13c5cb3ff0d427
Max <maxim00066@gmail.com>
2017-03-13 15:15:27 +0100

    commit

---
+++ src/blocks/input-caret/input-caret.js
@@ -0,0 +1,14 @@
+
+// Communicate to DOM that we have JS
+
+document.documentElement.setAttribute("class", "js");
+
+var searchFauxInput = document.querySelector(".input-caret__span");
+var searchBox = document.getElementById("Input");
+
+searchBox.addEventListener("keyup", function copyInput(event) {
+      searchFauxInput.textContent = searchBox.value;
+      searchBox.setAttribute("value", searchBox.value);
+}, false);
+
+//searchBox.focus();
